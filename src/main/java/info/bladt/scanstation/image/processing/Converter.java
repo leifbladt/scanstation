253dec114632fc100039d7c8b76c67e73927f2e6
Leif Bladt <leif.bladt@gmx.de>
2019-12-06 23:46:35 +0100

    Add converter (grayscale to binary)

---
+++ src/main/java/info/bladt/scanstation/image/processing/Converter.java
@@ -0,0 +1,18 @@
+package info.bladt.scanstation.image.processing;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Converter {
+
+    public static BufferedImage toBinary(BufferedImage input) {
+
+        BufferedImage binary = new BufferedImage(input.getWidth(), input.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
+
+        Graphics biG = binary.getGraphics();
+        biG.drawImage(input, 0, 0, null);
+        biG.dispose();
+
+        return binary;
+    }
+}
