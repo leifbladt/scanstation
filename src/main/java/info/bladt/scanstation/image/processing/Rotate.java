94817c244e30a5e091ddf39982e0c6dab437722d
Leif Bladt <leif.bladt@gmx.de>
2020-01-23 22:00:45 +0100

    Adjust output dimensions for rotated image

--- src/main/java/info/bladt/scanstation/image/processing/Rotate.java
+++ src/main/java/info/bladt/scanstation/image/processing/Rotate.java
@@ -7,18 +7,23 @@ public class Rotate {
 
     public BufferedImage process(BufferedImage input, double angle) {
 
+        double sin = Math.abs(Math.sin(Math.toRadians(angle)));
+        double cos = Math.abs(Math.cos(Math.toRadians(angle)));
         int w = input.getWidth();
         int h = input.getHeight();
+        int newW = (int) Math.floor(w * cos + h * sin);
+        int newH = (int) Math.floor(h * cos + w * sin);
 
-        BufferedImage rotated = new BufferedImage(w, h, input.getType());
+        BufferedImage rotated = new BufferedImage(newW, newH, input.getType());
 
         Graphics2D g = rotated.createGraphics();
 
         g.setPaint(new Color(255, 255, 255));
-        g.fillRect(0, 0, w, h);
+        g.fillRect(0, 0, newW, newH);
 
-        g.rotate(Math.toRadians(angle), w / 2d, h / 2d);
-        g.drawImage(input, null, 0, 0);
+        g.translate((newW - w) / 2, (newH - h) / 2);
+        g.rotate(Math.toRadians(angle), w / 2, h / 2);
+        g.drawRenderedImage(input, null);
         g.dispose();
 
         return rotated;
