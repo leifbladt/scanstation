72a2811bc833c791cf6bd0ebf17d4d66afa6f0b0
Leif Bladt <leif.bladt@gmx.de>
2020-01-26 01:21:48 +0100

    Update configurations

--- src/main/java/info/bladt/scanstation/image/SoundsOfSousaConfiguration.java
+++ src/main/java/info/bladt/scanstation/image/SoundsOfSousaConfiguration.java
@@ -3,6 +3,9 @@ package info.bladt.scanstation.image;
 import info.bladt.scanstation.image.export.ExportConfiguration;
 import info.bladt.scanstation.image.processing.ProcessingConfiguration;
 import info.bladt.scanstation.image.processing.RemoveEdges;
+import info.bladt.scanstation.model.Instrument;
+
+import static info.bladt.scanstation.model.Key.C;
 
 public class SoundsOfSousaConfiguration extends Configuration {
 
@@ -22,6 +25,12 @@ public class SoundsOfSousaConfiguration extends Configuration {
             setPageHeight(6052);
             setPageWidth(4272);
             setPaperEdgeWidth(new RemoveEdges.Width(70));
+
+            Instrument firstBass = new Instrument("1st Bass", C);
+            setCrop(firstBass, false);
+
+            Instrument secondBass = new Instrument("2nd Bass", C);
+            setCrop(secondBass, false);
         }
 
     }
@@ -32,6 +41,12 @@ public class SoundsOfSousaConfiguration extends Configuration {
             setPageOrientation(PageOrientation.PORTRAIT);
             setScaleToFit(false);
             setScale(72 / 600f * 1.1f);
+
+            Instrument firstBass = new Instrument("1st Bass", C);
+            setScale(72 / 600f, firstBass);
+
+            Instrument secondBass = new Instrument("2nd Bass", C);
+            setScale(72 / 600f, secondBass);
         }
     }
 }
