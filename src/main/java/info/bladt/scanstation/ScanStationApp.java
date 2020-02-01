25279f0807ce4960952f8b63833de27dbbce9dca
Leif Bladt <leif.bladt@gmx.de>
2020-02-01 01:31:34 +0100

    Introduce spring-boot

--- src/main/java/info/bladt/scanstation/ScanStationApp.java
+++ src/main/java/info/bladt/scanstation/ScanStationApp.java
@@ -7,19 +7,27 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+@SpringBootApplication
+@ComponentScan({"info.bladt.scanstation"})
 public class ScanStationApp extends Application {
 
     private static final Logger LOGGER = LogManager.getLogger(ScanStationApp.class);
+    private ConfigurableApplicationContext springContext;
+    private FXMLLoader fxmlLoader;
 
     @Override
     public void start(Stage primaryStage) {
 
         try {
-            FXMLLoader loader = new FXMLLoader(ScanStationApp.class.getResource("Main.fxml"));
+            fxmlLoader = new FXMLLoader(ScanStationApp.class.getResource("Main.fxml"));
 
             // Load scene
-            VBox pane = loader.load();
+            VBox pane = fxmlLoader.load();
             Scene scene = new Scene(pane);
             primaryStage.setScene(scene);
             primaryStage.setTitle("ScanStation");
@@ -28,6 +36,18 @@ public class ScanStationApp extends Application {
         } catch (Exception e) {
             LOGGER.error("Could not start application", e);
         }
+    }
+
+    @Override
+    public void init() throws Exception {
+        springContext = SpringApplication.run(ScanStationApp.class);
+        fxmlLoader = new FXMLLoader();
+        fxmlLoader.setControllerFactory(springContext::getBean);
+    }
+
+    @Override
+    public void stop() {
+        springContext.stop();
     }
 
     public static void main(String[] unused) {
